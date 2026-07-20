9ae18d1f57165dee2f58b76f70e09e9ffc91b0cf
Riley <rileymskcnatools@gmail.com>
2026-07-20 17:17:14 +0800

    chore: 更新练习代码 test_demo.py + 笔记同步

---
+++ 03-项目/PythonCode/ch06_函数/异常处理.py
@@ -0,0 +1,40 @@
+# file = open("data.txt","r")
+# try:
+#     # 写入数据时可能会有问题
+#     file.write("写入的数据")
+# except IOError as err:
+#     print("文件不能写入", err)
+#
+# file.close()
+
+
+# file = open("data.txt","r")
+# try:
+#     # 写入数据时可能会有问题
+#     # file.write("写入的数据")
+#     # print(a)
+#     print(3 / 0)
+#     # print([][10])
+#     print("hello" + 100)
+# except IOError as err:
+#     print("文件不能写入", err)
+# except NameError:
+#     print("标识符没有定义")
+# except ZeroDivisionError:
+#     print("除数不能为0")
+# except IndexError:
+#     print("下标越界了")
+# except Exception:
+#     print("程序运行出错，请检查代码")
+
+# Python 使用 else 在处理在代码无异常时的后续操作
+try:
+    n = input("请输入一个数字:")
+    num = int(n)
+except Exception:
+    print("元素无法转换为数字")
+else:
+    print("转换后成功", num)    # 只有转换成功才执行
+
+
+
