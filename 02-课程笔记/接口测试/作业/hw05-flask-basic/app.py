bbf1a5d0b2a9bfe570da4757546495a1e38dbb8f
Riley Xiong <rileymskcnctools@gmail.com>
2026-08-05 17:27:57 +0800

    feat: 建立 Pytest 作业目录 (02-课程笔记/Pytest/作业/)

---
+++ 02-课程笔记/接口测试/作业/hw05-flask-basic/app.py
@@ -0,0 +1,36 @@
+from flask import Flask
+
+
+from user import user_blueprint
+
+from task import task_blueprint
+
+
+
+app = Flask(__name__)
+
+
+
+# 注册蓝图
+
+app.register_blueprint(
+    user_blueprint
+)
+
+
+app.register_blueprint(
+    task_blueprint
+)
+
+
+
+@app.route("/")
+def index():
+
+    return "Flask Blueprint Demo"
+
+
+
+if __name__ == "__main__":
+
+    app.run(debug=True)
